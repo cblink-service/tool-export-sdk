2bc2cbe1439f351b22f7ebc53ae916f14ebe76aa
Nick <i@httpd.cc>
2022-08-18 15:44:26 +0800

    perf: 增加导出文件

--- src/Application.php
+++ src/Application.php
@@ -6,10 +6,12 @@ use Cblink\Service\Foundation\Container;
 
 /**
  * @property Task\Client $task  导出任务
+ * @property File\Client $file  导出文件
  */
 class Application extends Container
 {
     protected array $providers = [
         Task\ServiceProvider::class,
+        File\ServiceProvider::class,
     ];
 }
---
+++ src/File/Client.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the cblink/dispatch-meituan.
+ *
+ * (c) jinjun <757258777@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Cblink\Service\ExportTool\File;
+
+use Cblink\Service\ExportTool\Kernel\BaseApi;
+use GuzzleHttp\Exception\GuzzleException;
+use Psr\Http\Message\ResponseInterface;
+
+class Client extends BaseApi
+{
+    /**
+     * 获取下载地址
+     *
+     * @param int $id  文件ID
+     * @param array $params
+     * @return array|ResponseInterface|string
+     * @throws GuzzleException
+     */
+    public function download($id, array $params = [])
+    {
+        return $this->httpGet(sprintf('/api/file/%s/download', $id), $params);
+    }
+}
